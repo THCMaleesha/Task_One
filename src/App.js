ca1fa94eac0c313285b8718a16d09903e22d8bbf
THCMaleesha <thcmaleesha@gmail.com>
2022-12-11 12:27:36 +0530

    Get dog breeds using API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import {Form} from 'react-bootstrap';
+import Axios from 'axios';
 
 function App() {
+
+  const [breed, setBreed] = useState([])
+  useEffect(() => {
+    var URL = 'https://dog.ceo/api/breeds/list/all';
+    Axios 
+    .get(URL)
+    .then(res => {
+      for(var a in res.data.message) {
+        if(res.data.message[a].length !== 0)
+          for(var b of res.data.message[a])
+            breed.push(a + "/" + b)
+        else
+          breed.push(a)
+        setBreed(prev => [...breed])
+       }
+    })
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='app'>
+      <Form>
+        <Form.Group>
+          <Form.Control as='select'>
+            {breed.map(b => {
+              return <option key={b}>{b}</option>
+            })}
+          </Form.Control>
+        </Form.Group>
+      </Form>
+    </div>    
   );
 }
 
